3c0a0662a85b7a63a531eb2bb7002378e9725dde
Andressssssssss <andresduce118@hotmail.com>
2019-04-13 17:08:37 -0400

    subida curso laravel

---
+++ resources/views/contact/register.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('contact')
+
+<h1>Hola Isnoret</h1>
+
+@endsection
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Create Contact</div>
+
+                <div class="card-body">
+
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
